2e984915787a2f473d8669fe1351ac2bcbbbdb30
Owieoghene16 <kindnessokpugie1@gmail.com>
2023-06-02 18:50:45 +0100

    clean code

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,9 +1,7 @@
 module.exports.fileFilter = async (req, res, next) => {
-  console.log('its running')
   const imageFile = req.files[0];
   const secongimageFile = req.files[1];
   const thirdimageFile = req.files[2];
-  console.log(imageFile)
   // Accept images only
   if ( !imageFile || !secongimageFile || !thirdimageFile ) {
     return res.status(401).json({ message: 'Three images must be provided for this product' });
